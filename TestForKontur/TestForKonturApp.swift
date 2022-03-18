398037ddc45104a9c8c91bd49004867327870eb5
Domiik <memfis15@yandex.ru>
2022-03-18 13:05:05 +0300

    Initial Commit

---
+++ TestForKontur/TestForKonturApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestForKonturApp.swift
+//  TestForKontur
+//
+//  Created by Domiik on 18.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestForKonturApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
